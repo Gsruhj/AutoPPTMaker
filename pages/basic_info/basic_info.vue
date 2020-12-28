0a8e69254071db954e5d192cfb93d9cd4abc53ec
lpdink <326490366@qq.com>
2020-12-28 23:01:42 +0800

    init/创建主页面/创建init_info页面

---
+++ pages/basic_info/basic_info.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="content">
+		<view class="info-header">
+			<text class="header-logo">make your own PPT</text>
+			<view class="header-text-area">
+				<text class="header-text-selected">基本信息</text>
+				<text class="header-text">文档导入</text>
+				<text class="header-text">选择模板</text>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.content{
+		display: flex;
+		flex-direction: column;
+	}
+	
+	.info-header{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: #3fd5d5;
+		position: absolute;
+		height: 15%;
+		width: 100%;
+	}
+	
+	.header-logo{
+		display: block;
+		justify-content: center;
+		font-size: 100%;
+		color: #4bffff;
+	}
+	
+	.header-text-area{
+		display: flex;
+		flex-direction: row;
+		align-items: flex-end;
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+		
+	}
+	
+	.header-text{
+		color: #000000;
+		margin-left: 25rpx;
+		margin-bottom: 15rpx;
+		font-weight: 400;
+		font-size: 150%;
+		
+	}
+	
+	.header-text-selected{
+		color: #e6e6e6;
+		margin-left: 25rpx;
+		margin-bottom: 15rpx;
+		font-weight: 200;
+		font-size: 150%;
+		text-decoration:underline;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="content">
+		<text class="logo-font">Auto PPT</text>
+		<view class="image-area">
+			<image class="logo-begin" src="../../static/logo/begin.png" @tap="begin"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			begin(){
+				uni.navigateTo({
+					url:"../basic_info/basic_info"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		position: absolute;
+		flex-direction: column;
+		align-items: center;
+		background-color: #3fd5d5;
+		height: 100%;
+		width: 100%;
+	}
+	
+	.logo-font{
+		display: block;
+		font-size: 400%;
+		color: #4bffff;
+	}
+	
+	.image-area{
+		display: flex;
+		flex-direction: row;
+		position: relative;
+		height: 100%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.logo-begin{
+		width: 350rpx;
+		height: 350rpx;
+	}
+
+</style>
